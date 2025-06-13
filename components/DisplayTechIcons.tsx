071fcbbf423b65956ee1308c13431ce5f9443e17
dincho-kostadinov <kdincho@gmail.com>
2025-06-13 10:58:32 +0300

    home pages

---
+++ components/DisplayTechIcons.tsx
@@ -0,0 +1,35 @@
+import { cn, getTechLogos } from '@/lib/utils'
+import Image from 'next/image'
+import React from 'react'
+
+const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
+    /**
+     * Get tech logos by using utils function getTechLogos(techStack)
+     */
+    const techIcons = await getTechLogos(techStack)
+    return (
+        <div className="flex flex-row">
+            {techIcons.slice(0, 3).map(({ tech, url }, index) => (
+                <div
+                    key={tech}
+                    className={cn(
+                        "relative group bg-dark-300 rounded-full p-2 flex flex-center",
+                        index >= 1 && "-ml-2"
+                    )}
+                >
+                    <span className="tech-tooltip">{tech}</span>
+
+                    <Image
+                        src={url}
+                        alt={tech}
+                        width={80}
+                        height={80}
+                        className="size-5"
+                    />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default DisplayTechIcons
